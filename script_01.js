c26605d6912ed24c3a4eafc42d1613553ab1e0d8
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-21 12:30:14 +0200

    Auswahl Rechenarten ++

--- script_01.js
+++ script_01.js
@@ -4,9 +4,34 @@
 0. a+b / a-b/ a*b / a/b  // ergebnis c
 1. Dateneingabe + -überprüfung :: 
 2. Auswahl Rechenart :: 
-3. Fkt. Grundrechenarten :: 
-4. Ausgabe in Konsole :: 
+3. Fkt. Grundrechenarten :: erledigt
+4. Ausgabe in Konsole :: erledigt
 */
+
+
+/***** Auswahl der Rechenart *****/ 
+//** Tests **/
+// ausgabe (rechner("+",2,1));
+// ausgabe (rechner("-",2,1));
+//ausgabe (rechner("*",2,1));
+// ausgabe (rechner("/",2,1));
+// ausgabe (rechner("/",2,0)); // <-- geteilt durch 0
+//ausgabe (rechner("9",2,0)); // <-- ungültiger Operator
+// ausgabe (rechner("+",1)); // <-- Parameter fehlen
+function rechner (op,a,b) {    
+    switch (op) {
+        case "+":
+            return addieren(a,b);
+        case "-":
+            return subtrahieren(a,b);
+        case "*":
+            return multiplizieren(a,b);
+        case "/":
+            return dividieren(a,b);
+        default:
+            return "UPS! Es ist ein Fehler aufgetreten!"
+    }
+}
 
 
 /***** Grundrechenarten *****/ 
@@ -26,11 +51,12 @@ function multiplizieren(a,b) {
     return a * b;
 }
 /** Division a / b **/ 
-ausgabe (dividieren(1,0)); //<---- Test
+//ausgabe (dividieren(1,0)); //<---- Test
 function dividieren(a,b) {
     if (b!=0) {return a / b;}
     return "Dividieren durch 0 nicht möglich!";
 }
+
 
 /***** Ausgabe *****/ 
 // ausgabe("Hallo Welt") //<---- Test
